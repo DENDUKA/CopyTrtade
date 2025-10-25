e09a32dbc3f38e15b9b42cf7c5f8c440bac14fcc
DENDUKA <denduka@yandex.ru>
2025-10-25 20:20:09 +0300

    Update wallet list and price formatting
    Revised the list of tracked wallets in WalletSettings.cs, adding new wallets and removing some existing ones. Also updated OrdersTrades.razor to display price values without fixed decimal formatting.

--- CopyTrading/Settings/WalletSettings.cs
+++ CopyTrading/Settings/WalletSettings.cs
@@ -6,36 +6,31 @@ public static class WalletSettings
 {
     public static Wallet[] TrackedWallets =
     [
-        new Wallet("0x7bde2b9240a2ee352108c6823a9fa20f225b83a0"),
-        new Wallet("0x1e37a337ed460039d1b15bd3bc489de789768d5e"),
-        new Wallet("0x99c2cec0cd98df74fb14a8b7e6b71f0ebcc02600"),
-        new Wallet("0x582696ac2eb0559234d695452f3d76c4f6be96f6"),
-        new Wallet("0xc2d4ff17906940004b25884779c74aecda24fb83"),
-        new Wallet("0xdb6995164092fc54a0c33fdf032450d1f6f558a6"),
-        new Wallet("0x81a20870c5c7558f117166b2a598abaa8ce91f50"),
-        new Wallet("0xfb8194ca853afe29e17062b2cd60aeeaa4cfd907"),
-        new Wallet("0xa844d7ac9fa3424c4fd38a25baa23e460ec3e802"),
-        new Wallet("0x6217be04ad78ecaa59a6a165950c17f6f604927d"),
-        new Wallet("0xff9152cce6fbd30988b1aef70df6f086c99e2c55"),
-        new Wallet("0xf0f2712ff99ac60d69fc837a62e5d523a01ccf65"),
-        new Wallet("0xbb088e9852e18b1df8890d8d0f48bdcec0ab0964"),
-        new Wallet("0xf709deb9ca069e53a31a408fde397a87d025a352"),
-        new Wallet("0xb48cd87a34e4a756f03bc3d0f78ef470937ec9a6"),
-        new Wallet("0x8f8d2d2565bfb10608a7ce64b48e2aea7875a344"),
-        new Wallet("0xe61c2251b2641989f49bb5b73b2a8d0dbc6e40d8"),
-        new Wallet("0x685feceec46dd4e5c9b5b726f5d7550fd0eda526"),
-        new Wallet("0x33c3b8bcd758f511b350374a63e1f880f40e0282"),
-        new Wallet("0x8aa077f5998d234ac8641d73d6bc4976e2a210fc"),
+        ///Хороший :
+        new Wallet("0x6049ddfd35e4d9098a6367e36023f2a36792b642"),
+
+
+        //Рассматриваю
         new Wallet("0xee62db4851c3c6c5bf8010e839f2ae15fb4764bb"),
         new Wallet("0x987163b6b482c30c2f5f3aa2760109668eb0091d"),
         new Wallet("0xf770f371cc66499a89ae56aa84f9506e083f99ea"),
         new Wallet("0x6e4d47dad1e97833f4ecb0ef56347ba8e6fd1c0e"),
         new Wallet("0x491251909171d4b9b39c5bc5d70515292e21eaf0"),
 
-
         new Wallet("0xba939edf38c0ae0cc689c98b492e0535f43e4550"),
-        new Wallet("0x6049ddfd35e4d9098a6367e36023f2a36792b642"),
         new Wallet("0x5a8e21c3a73cc4742ecf1f422379a6fbf1992a42"),
+
+        new Wallet("0x1e37a337ed460039d1b15bd3bc489de789768d5e"),
+        new Wallet("0xc2d4ff17906940004b25884779c74aecda24fb83"),
+        new Wallet("0xdb6995164092fc54a0c33fdf032450d1f6f558a6"),
+        new Wallet("0xa844d7ac9fa3424c4fd38a25baa23e460ec3e802"),
+        new Wallet("0xff9152cce6fbd30988b1aef70df6f086c99e2c55"),
+        new Wallet("0xbb088e9852e18b1df8890d8d0f48bdcec0ab0964"),
+        new Wallet("0xf709deb9ca069e53a31a408fde397a87d025a352"),
+        new Wallet("0xb48cd87a34e4a756f03bc3d0f78ef470937ec9a6"),
+        new Wallet("0x685feceec46dd4e5c9b5b726f5d7550fd0eda526"),
+        new Wallet("0x33c3b8bcd758f511b350374a63e1f880f40e0282"),
+
 
 
     ];
